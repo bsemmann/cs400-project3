7b6efeda4a8a876a8b13085a0a0114c95299d407
Sriram Alla <63613553+SriramAlla123@users.noreply.github.com>
2020-11-10 12:00:12 -0600

    Update Cities.java

--- salla_Data_Wrangler_1/Cities.java
+++ salla_Data_Wrangler_1/Cities.java
@@ -14,11 +14,12 @@ public class Cities {
 	CS400Graph<String> cities; // graph to store the cities in our map
 
 	/**
-	 * Constructor to read through the text file and create a graph. This code
-	 * should work properly with all of our implementations of dijkstra's algorithm.
+	 * Constructor to read through the text file and create a graph of cities. This
+	 * code should work properly with all of our implementations of dijkstra's
+	 * algorithm.
 	 * 
 	 */
-	
+
 	public Cities() {
 		try {
 			cities = new CS400Graph<>();
@@ -29,12 +30,15 @@ public class Cities {
 				String names[] = line.split(","); // splitting the lines in the file which is comma-separated
 
 				if (!(cities.containsVertex(names[0])) || !(cities.containsVertex(names[1]))) { // add a vertex to the
-														// graph only if it does
-													        // not already exist
-														// in the graph to prevent duplicate vertices
+							               						// graph only if it does
+														// not already exist
+													        // in the graph to
+														// prevent duplicate
+																								// vertices
 					cities.insertVertex(names[0]);
 					cities.insertVertex(names[1]);
 				}
+
 				cities.insertEdge(names[0], names[1], Integer.parseInt(names[2])); // add the edges to the graph
 			}
 			sc.close();
